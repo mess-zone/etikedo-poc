20319affee41cb47df2582375df507da22d99269
Gilmar Andrade <gilmar-andrade@outlook.com>
2023-06-18 10:00:20 -0300

    fix:hanlde slider change

--- src/components/BottomControls.vue
+++ src/components/BottomControls.vue
@@ -4,12 +4,11 @@
             <svg class="play-icon" xmlns="http://www.w3.org/2000/svg" height="1em" viewBox="0 0 384 512"><!--! Font Awesome Free 6.4.0 by @fontawesome - https://fontawesome.com License - https://fontawesome.com/license (Commercial License) Copyright 2023 Fonticons, Inc. --><path d="M73 39c-14.8-9.1-33.4-9.4-48.5-.9S0 62.6 0 80V432c0 17.4 9.4 33.4 24.5 41.9s33.7 8.1 48.5-.9L361 297c14.3-8.7 23-24.2 23-41s-8.7-32.2-23-41L73 39z"/></svg>
             <svg class="pause-icon" xmlns="http://www.w3.org/2000/svg" height="1em" viewBox="0 0 320 512"><!--! Font Awesome Free 6.4.0 by @fontawesome - https://fontawesome.com License - https://fontawesome.com/license (Commercial License) Copyright 2023 Fonticons, Inc. --><path d="M48 64C21.5 64 0 85.5 0 112V400c0 26.5 21.5 48 48 48H80c26.5 0 48-21.5 48-48V112c0-26.5-21.5-48-48-48H48zm192 0c-26.5 0-48 21.5-48 48V400c0 26.5 21.5 48 48 48h32c26.5 0 48-21.5 48-48V112c0-26.5-21.5-48-48-48H240z"/></svg>
         </button>
-        <input type="number" v-model="currentTime" :min="min" :max="max" />
-        <Slider v-model="currentTime" :disabled="disabled" :min="min" :max="max"/>
+        <Slider v-model="currentTime" :disabled="disabled" :min="min" :max="max" @update:modelValue="handleChange"/>
     </div>
 </template>
 <script setup lang="ts">
-import { computed, ref } from 'vue';
+import { ref } from 'vue';
 import Slider from './Slider.vue';
 import { useVideoStore } from '../stores/video';
 import { storeToRefs } from 'pinia';
@@ -27,6 +26,10 @@ function togglePlay() {
     } else {
         videoStore.media.pause()
     }
+}
+
+function handleChange(value: number) {
+    videoStore.media.currentTime = value
 }
 </script>
 <style scoped>
