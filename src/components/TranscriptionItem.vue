f7999da97557c301f550806e4b3fffe94270ff15
Gilmar Andrade <gilmar-andrade@outlook.com>
2023-06-18 18:28:24 -0300

    feat: style transcriptionItem avatar

--- src/components/TranscriptionItem.vue
+++ src/components/TranscriptionItem.vue
@@ -1,18 +1,27 @@
 <template>
-    <div class="transcription-item" :class="{ 'is-active': transcription.isActive }" @click="handleClick">
-        <textarea ref="textInput" class="text" v-model="transcription.cue.text" @input="resizeInput($event.target)"></textarea>
-        <header>
-            <span class="timestamp">
-                <!-- <button @click="decrementStart">-</button>
-                <button @click="incrementStart">+</button> -->
-                {{ formatedStart }} - {{ formatedEnd }}
-                <!-- <button @click="decrementEnd">-</button>
-                <button @click="incrementEnd">+</button> -->
-            </span>
-            <button class="btn-remove" @click="handleRemove(transcription, $event)">
-                <svg xmlns="http://www.w3.org/2000/svg" height="1em" viewBox="0 0 448 512"><!--! Font Awesome Free 6.4.0 by @fontawesome - https://fontawesome.com License - https://fontawesome.com/license (Commercial License) Copyright 2023 Fonticons, Inc. --><path d="M170.5 51.6L151.5 80h145l-19-28.4c-1.5-2.2-4-3.6-6.7-3.6H177.1c-2.7 0-5.2 1.3-6.7 3.6zm147-26.6L354.2 80H368h48 8c13.3 0 24 10.7 24 24s-10.7 24-24 24h-8V432c0 44.2-35.8 80-80 80H112c-44.2 0-80-35.8-80-80V128H24c-13.3 0-24-10.7-24-24S10.7 80 24 80h8H80 93.8l36.7-55.1C140.9 9.4 158.4 0 177.1 0h93.7c18.7 0 36.2 9.4 46.6 24.9zM80 128V432c0 17.7 14.3 32 32 32H336c17.7 0 32-14.3 32-32V128H80zm80 64V400c0 8.8-7.2 16-16 16s-16-7.2-16-16V192c0-8.8 7.2-16 16-16s16 7.2 16 16zm80 0V400c0 8.8-7.2 16-16 16s-16-7.2-16-16V192c0-8.8 7.2-16 16-16s16 7.2 16 16zm80 0V400c0 8.8-7.2 16-16 16s-16-7.2-16-16V192c0-8.8 7.2-16 16-16s16 7.2 16 16z"/></svg>
-            </button>
-        </header>
+    <div class="transcription-block" :class="{ 'is-active': transcription.isActive }">
+        <div class="speaker">
+            <div class="avatar">a</div>
+        </div>
+        <div class="transcription-item" @click="handleClick">
+            <header>
+                <span class="author">antonio</span>
+                <div class="actions">
+                    <span class="timestamp">
+                        <!-- <button @click="decrementStart">-</button>
+                        <button @click="incrementStart">+</button> -->
+                        {{ formatedStart }} - {{ formatedEnd }}
+                        <!-- <button @click="decrementEnd">-</button>
+                        <button @click="incrementEnd">+</button> -->
+                    </span>
+                    <button class="btn-remove" @click="handleRemove(transcription, $event)">
+                        <svg xmlns="http://www.w3.org/2000/svg" height="1em" viewBox="0 0 448 512"><!--! Font Awesome Free 6.4.0 by @fontawesome - https://fontawesome.com License - https://fontawesome.com/license (Commercial License) Copyright 2023 Fonticons, Inc. --><path d="M170.5 51.6L151.5 80h145l-19-28.4c-1.5-2.2-4-3.6-6.7-3.6H177.1c-2.7 0-5.2 1.3-6.7 3.6zm147-26.6L354.2 80H368h48 8c13.3 0 24 10.7 24 24s-10.7 24-24 24h-8V432c0 44.2-35.8 80-80 80H112c-44.2 0-80-35.8-80-80V128H24c-13.3 0-24-10.7-24-24S10.7 80 24 80h8H80 93.8l36.7-55.1C140.9 9.4 158.4 0 177.1 0h93.7c18.7 0 36.2 9.4 46.6 24.9zM80 128V432c0 17.7 14.3 32 32 32H336c17.7 0 32-14.3 32-32V128H80zm80 64V400c0 8.8-7.2 16-16 16s-16-7.2-16-16V192c0-8.8 7.2-16 16-16s16 7.2 16 16zm80 0V400c0 8.8-7.2 16-16 16s-16-7.2-16-16V192c0-8.8 7.2-16 16-16s16 7.2 16 16zm80 0V400c0 8.8-7.2 16-16 16s-16-7.2-16-16V192c0-8.8 7.2-16 16-16s16 7.2 16 16z"/></svg>
+                    </button>
+                </div>
+               
+            </header>
+            <textarea ref="textInput" class="text" v-model="transcription.cue.text" @input="resizeInput($event.target)"></textarea>
+    </div>
 
     </div>
 </template>
@@ -129,8 +138,11 @@ onMounted(() => {
 
 <style scoped>
 
-.transcription-item {
-    background-color: rgba(255, 192, 203, 0.616);
+.transcription-block {
+    display: flex;
+    align-items: flex-start;
+    gap: 10px;
+
     padding: 10px 15px;
     border-radius: 8px;
     margin-bottom: 10px;
@@ -139,8 +151,47 @@ onMounted(() => {
     cursor: pointer;
 }
 
-.transcription-item:hover {
-    background-color: rgba(255, 192, 203, 0.85);
+.transcription-item {
+    flex-basis: 100%;
+    /* background-color: rgba(255, 192, 203, 0.616); */
+    /* padding: 0px 15px; */
+    /* border-radius: 8px; */
+    /* margin-bottom: 10px; */
+    /* box-sizing: border-box; */
+    /* cursor: pointer; */
+}
+
+.transcription-block:hover {
+    background-color: rgba(255, 192, 203, 0.507);
+}
+
+.speaker {
+    display: flex;
+    align-items: center;
+    gap: 10px;
+    /* padding-top: 10px; */
+}
+.author {
+    font-weight: bold;
+    font-size: .8em;
+}
+.avatar {
+    width: 32px;
+    height: 32px;
+    background-color: turquoise;
+    border-radius: 50%;
+    display: flex;
+    justify-content: center;
+    align-items: center;
+    color: white;
+    text-transform: uppercase;
+}
+
+.actions {
+    display: flex;
+    align-items: center;
+    justify-content: right;
+    gap: 10px;
 }
 
 header {
@@ -156,7 +207,7 @@ header {
     font-size: 1em;
     font-family: sans-serif;
     margin: 0;
-    margin-top: 5px;
+    /* margin-top: 5px; */
     line-height: 1.5;
     white-space: break-spaces;
     display: block;
@@ -164,10 +215,11 @@ header {
     background-color: transparent;
     border: none;
     min-height: 18px;
+    /* margin-bottom: 5px; */
 }
 
-.transcription-item.is-active {
-    background-color: rgba(255, 113, 184, 0.842);
+.transcription-block.is-active {
+    background-color: rgba(255, 113, 184, 0.226);
     border-color: rgba(240, 0, 120, 0.842);
 }
 
