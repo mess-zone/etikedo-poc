21e5867985708c2f3599c41ad7d0bea222c2ec2a
Gilmar Andrade <gilmar-andrade@outlook.com>
2023-06-18 20:55:46 -0300

    feat: random color for avatars

--- src/components/TranscriptionItem.vue
+++ src/components/TranscriptionItem.vue
@@ -1,7 +1,7 @@
 <template>
     <div class="transcription-block" :class="{ 'is-active': transcription.isActive }">
         <div class="speaker">
-            <div class="avatar">{{transcription.speaker}}</div>
+            <div class="avatar" :style="{ backgroundColor: stringToHexColor(''.padStart(5, transcription.speaker))}">{{transcription.speaker.charAt(0)}}</div>
         </div>
         <div class="transcription-item" @click="handleClick">
             <header>
@@ -38,7 +38,23 @@ const props = defineProps<{
     transcription: transcriptionCue
 }>()
 
+function stringToHexColor(str: string): string {
+  let hash = 0;
 
+  for (let i = 0; i < str.length; i++) {
+    hash = str.charCodeAt(i) + ((hash << 5) - hash);
+    hash = hash & hash; // Converte para um inteiro de 32 bits
+  }
+
+  let color = "#";
+
+  for (let i = 0; i < 3; i++) {
+    const value = (hash >> (i * 8)) & 0xff;
+    color += ("00" + value.toString(16)).substr(-2);
+  }
+
+  return color;
+}
 
 const videoStore = useAudioStore()
 
