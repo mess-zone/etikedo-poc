3f880e5917153bf43b2ed0997554e91e6ae1f0cc
Gilmar Andrade <gilmar-andrade@outlook.com>
2023-06-19 16:32:07 -0300

    feat: useTrack add and remove utterances

--- src/composables/track.ts
+++ src/composables/track.ts
@@ -1,19 +1,89 @@
-import { ref } from "vue"
+import { Ref, isRef, ref, toValue, unref } from "vue"
+import { v4 as uuidv4 } from 'uuid';
 
-export type IUtterance = {
-    id: number,
+export interface UtteranceData {
+    id: string,
     text: string,
-    speaker: string,
-    start: string,
-    end: string,
-    display: string,
+    start: number,
+    end: number,
+    speaker?: number,
+    layout?: 'BLOCK' | 'INLINE',
 }
+
+const sample: UtteranceData[] = [
+    {
+        id: '0',
+        text: "Lorem ipsum dolor sit amet, consectetur adipisicing elit. Sed dolorem, autem voluptas error eaque neque! Ipsam, deserunt officiis fugiat laborum enim libero corrupti id, optio iste accusantium odit natus non? Lorem ipsum dolor sit amet, consectetur adipisicing elit. Sed dolorem, autem voluptas error eaque neque! Ipsam, deserunt officiis fugiat laborum enim libero corrupti id, optio iste accusantium odit natus non?",
+        speaker: 0,
+        start: 2,
+        end: 3.5,
+        layout: 'BLOCK',
+    },
+    {
+        id: '1',
+        text: "Deu certo mesmo?",
+        speaker: 1,
+        start: 4,
+        end: 9,
+        layout: 'BLOCK',
+    },
+    {
+        id: '2',
+        text: "Lorem ipsum dolor sit amet, consectetur adipisicing elit. Sed dolorem, autem voluptas error eaque neque! Ipsam, deserunt officiis fugiat laborum enim libero corrupti id, optio iste accusantium odit natus non?",
+        speaker: 2,
+        start: 6,
+        end: 8.5,
+        layout: 'BLOCK',
+    },
+    {
+        id: '3',
+        text: "Lorem ipsum dolor sit amet, consectetur adipisicing elit",
+        speaker: 2,
+        start: 6.9,
+        end: 8,
+        layout: 'INLINE',
+    },
+    {
+        id: '4',
+        text: "ed dolorem,",
+        speaker: 0,
+        start: 8,
+        end: 9,
+        layout: 'INLINE',
+    },
+    {
+        id: '5',
+        text: "autem voluptas error eaque neque",
+        speaker: 2,
+        start: 8,
+        end: 9,
+        layout: 'INLINE',
+    },
+]
 
 export function useTrack() {
 
     const id = ref('track-id')
 
+    const utterances = ref<UtteranceData[]>(sample)
+
+    function addUtterance(item: UtteranceData | Ref<UtteranceData>) {
+        const i = toValue(item)
+        i.id = uuidv4()
+        utterances.value.push(i)
+    }
+
+    function removeUtterance(item: UtteranceData) {
+        const index = utterances.value.indexOf(toValue(item));
+        if (index > -1) {
+            utterances.value.splice(index, 1);
+        }
+    }
+
     return {
         id,
+        utterances,
+        addUtterance,
+        removeUtterance,
     }
 }
--- src/views/TestComponentsView.vue
+++ src/views/TestComponentsView.vue
@@ -1,8 +1,71 @@
 <template>
-    track id: <input type="text" v-model="id"> {{ id }}
+    <h1>
+        track id: <input type="text" v-model="id">
+    </h1>
+
+    <div>
+        <label>
+            text
+            <input type="text" v-model="utteranceToAdd.text">
+        </label>
+        <label>
+            start
+            <input type="number" v-model="utteranceToAdd.start">
+        </label>
+        <label>
+            end
+            <input type="number" v-model="utteranceToAdd.end">
+        </label>
+        <label>
+            speaker
+            <input type="number" v-model="utteranceToAdd.speaker">
+        </label>
+        <label>
+            layout
+            <input type="text" v-model="utteranceToAdd.layout">
+        </label>
+        <button @click="handleAddClick">add</button>
+    </div>
+    
+    <ul>
+        <li v-for="item in utterances" :key="item.id">
+            <pre>{{ item }}</pre>
+            <button @click="handleRemoveClick(item)">remove</button>
+        </li>
+    </ul>
 </template>
 <script setup lang="ts">
-import { useTrack } from '../composables/track';
+import { ref } from 'vue';
+import { UtteranceData, useTrack } from '../composables/track';
 
-const { id } = useTrack()
+const { id, utterances, removeUtterance, addUtterance } = useTrack()
+
+const utteranceToAdd = ref<UtteranceData>({
+    id: null,
+    text: '[EMPTY]',
+    start: 0,
+    end: 0,
+    speaker: 0,
+    layout: 'BLOCK',
+})
+
+function handleAddClick() {
+    console.log('add', utteranceToAdd.value)
+    
+    addUtterance(utteranceToAdd)
+
+    utteranceToAdd.value = {
+        id: null,
+        text: '[EMPTY]',
+        start: 0,
+        end: 0,
+        speaker: 0,
+        layout: 'BLOCK',
+    }
+}
+
+function handleRemoveClick(item: UtteranceData) {
+    console.log('remove', item)
+    removeUtterance(item)
+}
 </script>
