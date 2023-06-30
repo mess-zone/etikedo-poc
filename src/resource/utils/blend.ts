98ba99f55388a2e615a68e20e80a5473ca539f35
Gilmar Andrade <gilmar-andrade@outlook.com>
2023-06-30 17:30:15 -0300

    test: blend when Set has only one interval

--- src/resource/utils/blend.ts
+++ src/resource/utils/blend.ts
@@ -1,5 +1,20 @@
 import { Interval, fuse } from "./fuse"
 
-export function blend(C: Interval, B: Interval) {
-    return fuse(C, B)
+export function blend(interval: Interval, set: Interval[]) {
+
+    const c: Interval = {
+        start: interval.start,
+        end: interval.end,
+        isHigh: true
+    }
+    if(!set.length) {
+        const empty: Interval = {
+            start: 0,
+            end: interval.end,
+            isHigh: false,
+        }
+        return fuse(c, empty)
+    }
+    
+    return fuse(c, set[0])
 }
--- test/blend.spec.ts
+++ test/blend.spec.ts
@@ -1,25 +1,55 @@
 import { blend } from "../src/resource/utils/blend"
-import { fuse } from "../src/resource/utils/fuse"
-jest.mock('../src/resource/utils/fuse');
-const mockedFoo = jest.mocked(fuse)
+import { Interval, fuse } from "../src/resource/utils/fuse"
+// jest.mock('../src/resource/utils/fuse');
+// const mockedFuse = jest.mocked(fuse)
 
-describe('Blend intreval into array of intervals', () => {
-    test('C is empty (C ends before B)', () => {
-        const C = { start: -1, end: -1, isHigh: true }
-        const B = { start: 0, end: 5, isHigh: false }
+describe('Blend interval into array of intervals', () => {
 
-        mockedFoo.mockImplementation((c, b) => {
-            console.log(c, b)
-            C.start = 2
-            C.end = 2
-            return [{ start: 0, end: 5, isHigh: false }]
+    describe('empty set', () => {
+
+        test('C start equal 0', () => {
+            const set: Interval[] = []
+            const C: Interval = { start: 0, end: 2, isHigh: true }
+    
+            const result = blend(C, set)
+        
+            expect(result.length).toBe(1)
+            expect(result[0]).toEqual({ start: 0, end: 2, isHigh: true })
         })
 
-
-        const result = blend(C, B)
+        test('C start after 0', () => {
+            const set: Interval[] = []
+            const C: Interval = { start: 2, end: 5, isHigh: true }
     
-        expect(result.length).toBe(1)
-        expect(result[0]).toEqual({ start: 0, end: 5, isHigh: false })
-        expect(C).toEqual({ start: 2, end: 2, isHigh: true })
+            const result = blend(C, set)
+        
+            expect(result.length).toBe(2)
+            expect(result[0]).toEqual({ start: 0, end: 2, isHigh: false })
+            expect(result[1]).toEqual({ start: 2, end: 5, isHigh: true })
+        })
+    })
+
+    describe('set with one interval', () => {
+        test('C start equal 0', () => {
+            const set: Interval[] = [{ start: 0, end: 5, isHigh: false }]
+            const C: Interval = { start: 0, end: 2, isHigh: true }
+    
+            const result = blend(C, set)
+        
+            expect(result.length).toBe(2)
+            expect(result[0]).toEqual({ start: 0, end: 2, isHigh: true })
+            expect(result[1]).toEqual({ start: 2, end: 5, isHigh: false })
+        })
+        test('C start after 0', () => {
+            const set: Interval[] = [{ start: 0, end: 9, isHigh: false }]
+            const C: Interval = { start: 2, end: 5, isHigh: true }
+    
+            const result = blend(C, set)
+        
+            expect(result.length).toBe(3)
+            expect(result[0]).toEqual({ start: 0, end: 2, isHigh: false })
+            expect(result[1]).toEqual({ start: 2, end: 5, isHigh: true })
+            expect(result[2]).toEqual({ start: 5, end: 9, isHigh: false })
+        })
     })
 })
